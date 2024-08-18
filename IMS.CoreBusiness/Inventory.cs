98898e2e2dc9abab11b73d717b634baf4a0eabb4
jrickey24 <jrick26@wgu.edu>
2024-08-18 12:57:08 -0500

    Initial commit

---
+++ IMS.CoreBusiness/Inventory.cs
@@ -0,0 +1,10 @@
+﻿namespace IMS.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        public string InventoryName { get; set; } = string.Empty; // we never want this to be null so we initialize with empty string
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ IMS.Plugins/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.InMemory
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private List<Inventory> _inventories;
+        public InventoryRepository()
+        {
+            _inventories = new List<Inventory>()
+            { 
+                new Inventory{InventoryId = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2},
+                new Inventory{InventoryId = 2, InventoryName = "Bike Body", Quantity = 10, Price = 15},
+                new Inventory{InventoryId = 3, InventoryName = "Bike Wheels", Quantity = 20, Price = 8},
+                new Inventory{InventoryId = 4, InventoryName = "Bike Pedals", Quantity = 20, Price = 1},
+            };
+        }
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) return await Task.FromResult(_inventories);
+
+            return _inventories.Where(x => x.InventoryName.Contains(name, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
